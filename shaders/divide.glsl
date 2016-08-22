a05f88a623b7c94cf4b8dc3695167d1ac59e4db7
Pablo Riera <pablo.riera@gmail.com>
2016-08-22 15:40:10 -0300

    vamos

---
+++ shaders/divide.glsl
@@ -0,0 +1,17 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform float time;
+uniform vec2 mouse;
+uniform vec2 resolution;
+
+#define divide(what, part) floor(what * part) / part
+
+void main( void ) {
+
+	vec2 uv = gl_FragCoord.xy / min(resolution.x , resolution.y);
+	uv = divide(uv, 5.0);
+	float h = uv.y;
+	
+	gl_FragColor = vec4(h);
